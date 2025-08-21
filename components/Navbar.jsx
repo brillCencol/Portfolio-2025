ee8265d5d1fdc2e6e5baebe1649a058aab43b68f
brillCencol <btorino@my.centennialcollege.ca>
2025-08-20 21:51:48 -0400

    fix: profile picture overlapping. start About me

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,12 +1,10 @@
-import { assets } from '@/assets/assets'
+import React, { useRef } from 'react'
 import Image from 'next/image'
-import React, { useEffect, useRef, useState } from 'react'
+import { assets } from '@/assets/assets'
+const Navbar = () => {
 
-const Navbar = ({isDarkMode, setIsDarkMode}) => {
-
-    const [isScroll, setIsScroll] = useState(false)
     const sideMenuRef = useRef();
-
+    
     const openMenu = ()=>{
         sideMenuRef.current.style.transform = 'translateX(-16rem)'
     }
@@ -14,66 +12,52 @@ const Navbar = ({isDarkMode, setIsDarkMode}) => {
         sideMenuRef.current.style.transform = 'translateX(16rem)'
     }
 
-    useEffect(()=>{
-        window.addEventListener('scroll', ()=>{
-            if(scrollY > 50){
-                setIsScroll(true)
-            }else{
-                setIsScroll(false)
-            }
-        })
-    },[])
-
   return (
     <>
-    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%] dark:hidden'>
-       <Image src={assets.header_bg_color} alt='' className='w-full' />
+    <div className='fixed top-0 right-0 w-11/12 -z-10 translate-y-[-80%]'>
+        <Image src={assets.header_bg_color} alt='Header Background Color' className='w-full'/>
     </div>
+        <nav className='w-full fixed h-20 px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50'>
+            <a href="#top">
+                <Image src={assets.logo} alt="Logo" className='w-28 cursor-pointer mr-14'/>
+            </a>
 
-      <nav className={`w-full fixed px-5 lg:px-8 xl:px-[8%] py-4 flex items-center justify-between z-50 ${isScroll ? "bg-white bg-opacity-50 backdrop-blur-lg shadow-sm dark:bg-darkTheme dark:shadow-white/20" : ""}`}>
-        <a href="#top">
-            <Image src={isDarkMode ? assets.logo_dark : assets.logo} alt='' className='w-28 alt="" cursor-pointer mr-14'/>
-        </a>
+            <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 bg-white shadow-sm bg-opacity-50`}>
+                <li><a className='font-Ovo' href="#top">Home</a></li>
+                <li><a className='font-Ovo' href="#about">About me</a></li>
+                <li><a className='font-Ovo' href="#services">Services</a></li>
+                <li><a className='font-Ovo' href="#work">My Work</a></li>
+                <li><a className='font-Ovo' href="#contact">Contact me</a></li>
+            </ul>
 
-        <ul className={`hidden md:flex items-center gap-6 lg:gap-8 rounded-full px-12 py-3 ${isScroll ? "" : "bg-white shadow-sm bg-opacity-50 dark:border dark:border-white/50 dark:bg-transparent"} `}>
-            <li><a className='font-Ovo' href="#top">Home</a></li>
-            <li><a className='font-Ovo' href="#about">About me</a></li>
-            <li><a className='font-Ovo' href="#services">Services</a></li>
-            <li><a className='font-Ovo' href="#work">My Work</a></li>
-            <li><a className='font-Ovo' href="#contact">Contact me</a></li>
-        </ul>
+            <div className='flex items-center gap-4'>
 
-        <div className='flex items-center gap-4'>
+                <button>
+                    <Image src={assets.moon_icon} alt='Moon Icon' className='w-6'/>
+                </button>
 
-            <button onClick={()=> setIsDarkMode(prev => !prev)}>
-                <Image src={isDarkMode ? assets.sun_icon : assets.moon_icon} alt='' className='w-6' />
-            </button>
+                <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Contact <Image src={assets.arrow_icon} alt="Arrow Icon" className='w-3'/></a>
 
-            <a href="#contact" className='hidden lg:flex items-center gap-3 px-10 py-2.5 border border-gray-500 rounded-full ml-4 font-Ovo dark:border-white/50'>Contact 
-            <Image src={isDarkMode ? assets.arrow_icon_dark : assets.arrow_icon} alt="" className='w-3'/></a>
-
-            <button className='block md:hidden ml-3' onClick={openMenu}>
-            <Image src={isDarkMode ? assets.menu_white : assets.menu_black} alt='' className='w-6' />
-            </button>
-        </div>
-
-        {/* -- ----- mobile menu ------  -- */}
-
-        <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500 dark:bg-darkHover dark:text-white'>
-
-            <div className='absolute right-6 top-6' onClick={closeMenu}>
-                <Image src={isDarkMode ? assets.close_white : assets.close_black} alt='' className='w-5 cursor-pointer' />
+                <button className='block md:hidden ml-3' onClick={openMenu}>
+                    <Image src={assets.menu_black} alt='Menu Icon' className='w-6'/>
+                </button>
             </div>
 
-            <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href="#about">About me</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href="#services">Services</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href="#work">My Work</a></li>
-            <li><a className='font-Ovo' onClick={closeMenu} href="#contact">Contact me</a></li>
-        </ul>
+            {/* -- ----- mobile menu ------  -- */}
 
+            <ul ref={sideMenuRef} className='flex md:hidden flex-col gap-4 py-20 px-10 fixed -right-64 top-0 bottom-0 w-64 z-50 h-screen bg-rose-50 transition duration-500'>
 
-      </nav>
+                <div className='absolute right-6 top-6' onClick={closeMenu}>
+                    <Image src={assets.close_black} alt='Close Icon' className='w-5 cursor-pointer'/>
+                </div>
+
+                <li><a className='font-Ovo' onClick={closeMenu} href="#top">Home</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#about">About me</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#services">Services</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#work">My Work</a></li>
+                <li><a className='font-Ovo' onClick={closeMenu} href="#contact">Contact me</a></li>
+            </ul>
+        </nav>
     </>
   )
 }
